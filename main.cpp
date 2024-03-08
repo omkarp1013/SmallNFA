ef1ad2ac6a2135eb2e42ff2ea941822fd17e4161
pathmarkoak <omkarp321@gmail.com>
2024-03-07 20:23:05 -0500

    gitignore

--- main.cpp
+++ main.cpp
@@ -1,10 +1,20 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 
 int main() {
     int n;
     cin >> n;
+    
+    
+}
 
+vector<int> primes(int n) {
+    vector<int> res;
+    for(int i=0; i<)
+}
+
+bool is_prime(int n) {
     
 }
