1a6206c422de8a70e95513874972cd67046a29e3
pathmarkoak <omkarp321@gmail.com>
2024-03-08 13:03:37 -0500

    prog

--- main.cpp
+++ main.cpp
@@ -6,15 +6,19 @@ using namespace std;
 int main() {
     int n;
     cin >> n;
-    
+
+    int* p = primes(n);
+
     
 }
 
-vector<int> primes(int n) {
+int* primes(int n) {
+    // Finds primes p1, p2, ..., pl
     vector<int> res;
-    for(int i=0; i<)
 }
 
 bool is_prime(int n) {
-    
+    // Tests if a number is prime
+    (for int i = 0; i < )
 }
+
